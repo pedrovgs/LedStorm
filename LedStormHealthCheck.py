a3c0898908dd3706a476da4fbfdb9792c0259a71
Pedro Gómez <pedro@karumi.com>
2021-07-03 14:16:21 +0200

    Improve health check script

--- LedStormHealthCheck.py
+++ LedStormHealthCheck.py
@@ -1,13 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+
 def blink():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(18,GPIO.OUT)
     print ("LED on")
-    GPIO.output(18,GPIO.HIGH)
     time.sleep(1)
+    GPIO.output(18,GPIO.HIGH)
     print ("LED off")
     time.sleep(1)
     GPIO.output(18,GPIO.LOW)
