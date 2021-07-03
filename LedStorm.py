da3a3bcefea3edb668390093cf932b312d34a795
Pedro Gómez <pedro@karumi.com>
2021-07-03 15:41:55 +0200

    Add logs

--- LedStorm.py
+++ LedStorm.py
@@ -17,28 +17,28 @@ def distance():
     print ("Starting measure process")
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
- 
+    print ("Trigger pin set to true")
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
- 
+    print ("Trigger pin set to false")
     StartTime = time.time()
     StopTime = time.time()
- 
+    print ("Waiting for first echo value")
     # save StartTime
     while GPIO.input(GPIO_ECHO) == 0:
         StartTime = time.time()
- 
+    print ("Waiting for last echo value")
     # save time of arrival
     while GPIO.input(GPIO_ECHO) == 1:
         StopTime = time.time()
- 
+    print ("Waiting echo value received")
     # time difference between start and arrival
     TimeElapsed = StopTime - StartTime
     # multiply with the sonic speed (34300 cm/s)
     # and divide by 2, because there and back
     distance = (TimeElapsed * 34300) / 2
-    print ("Meassure process finished")
+    print ("Meassure process finished with distance = %.1f cm" % dist)
     return distance
  
 if __name__ == '__main__':
