d5777fbaac050a637d03ac46e658f0979318ba60
Pedro Gómez <pedro@karumi.com>
2021-07-16 16:22:36 +0200

    Add guard to ensure we are not blocking reading values

--- proximity_sensor.py
+++ proximity_sensor.py
@@ -29,11 +29,17 @@ def read_distance_using_time_elapsed_between_echos():
     start_time = time.time()
     stop_time = time.time()
     print("Starting distance read")
+    start_read_time = time.time()
     while GPIO.input(GPIO_ECHO) == 0:
         start_time = time.time()
+        if (time.time() - start_read_time > 1):
+            break
 
+    start_read_time = time.time()
     while GPIO.input(GPIO_ECHO) == 1:
         stop_time = time.time()
+        if (time.time() - start_read_time > 1):
+            break
 
     time_elapsed = stop_time - start_time
     # multiply with the sonic speed (34300 cm/s)
