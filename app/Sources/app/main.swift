b5f2dc654a9c34154e7e3310992d450f7cf7eac8
Pedro Gómez <pedro@karumi.com>
2021-07-20 19:35:50 +0200

    Prepare the follower's screen to be able to show any klisst and user followers

--- app/Sources/app/main.swift
+++ app/Sources/app/main.swift
@@ -1,17 +1,40 @@
 import TokamakDOM
 
 struct LedStormApp: App {
-    var body: some Scene {
-        WindowGroup("Tokamak App") {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup("Tokamak App") {
+      LedStormView()
     }
+  }
 }
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+struct LedStormView: View {
+  @State private var count: Int = 0
+  var body: some View {
+    VStack {
+      Spacer()
+      Button(
+        action: {
+          print("CLICK")
+        },
+        label: {
+          Text("⚡️").font(.system(size: 160))
+        }
+      ).buttonStyle(TransparentButtonStyle())
     }
+  }
+}
+
+struct TransparentButtonStyle: ButtonStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    let isPressed = configuration.isPressed
+    return configuration
+      .label.background(isPressed ? Color.buttonPressed : Color.clear)
+  }
+}
+
+extension Color {
+  static let buttonPressed = Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 1.0)
 }
 
 LedStormApp.main()
--- app/Tests/appTests/XCTestManifests.swift
+++ app/Tests/appTests/XCTestManifests.swift
@@ -1,9 +1,9 @@
 import XCTest
 
 #if !canImport(ObjectiveC)
-    public func allTests() -> [XCTestCaseEntry] {
-        return [
-            testCase(appTests.allTests),
-        ]
-    }
+public func allTests() -> [XCTestCaseEntry] {
+  [
+    testCase(appTests.allTests),
+  ]
+}
 #endif
